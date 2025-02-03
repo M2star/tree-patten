da3e35eacb419d0f6912526a8e1637dfe5b08eac
tushar <tushar@zithara.in>
2025-02-03 11:31:53 +0530

    changes

--- app/nested.tsx
+++ app/nested.tsx
@@ -1,46 +1,108 @@
 import React from "react";
 
-import  {
-    ActionButton,
-    TimerButton,
-    Tree,
-    TreeItemWrapper,
-    TwoBranchTree,
-    ActionState
-  } from "./tree";
-function Nested(data) {
+import {
+  ActionButton,
+  TimerButton,
+  Tree,
+  TreeItemWrapper,
+  TwoBranchTree,
+  ActionState,
+} from "./tree";
+import { RiTimerLine } from "react-icons/ri";
+import { MdNotInterested } from "react-icons/md";
+import { cn } from "@/lib/utils";
+function Nested({ data }) {
   return (
-    <Tree>
-      <TwoBranchTree>
-        <TreeItemWrapper>
-          <ActionState>
-            <span>
-              <MdNotInterested />
-            </span>
-            <span className="action-state__text">accepted</span>
-          </ActionState>
-          <TimerButton label="1" />
-          <div>
-            <Button>new sequence</Button>
+    <span role="tree" aria-label="source tree" className="action">
+      <div role="treeitem">
+        <div
+          role="group"
+          className={cn(" relative items-auto gap-0", data.noDelay ? "grid action__container--grid grid-cols-[auto_auto] " : "flex flex-col items-center" )}
+        >
+          {/*main left brach */}
+          <div role="tree-item" className="relative flex flex-col items-center">
+            <div className="relative">
+              <div className="w-40 action-delay relative mb-[var(--vertical-offset)] flex flex-col items-center">
+                <button className="text-center px-5 flex items-center justify-center gap-2 bg-gray-300 p-3 rounded w-40 relative">
+                  <span className="absolute left-5">
+                    <RiTimerLine className="mr-5" />
+                  </span>
+                  <span>{data.yesDelay?.value}</span>
+                  <span>{data.yesDelay?.timeUnit}</span>
+                </button>
+              </div>
+            </div>
+            <div
+              aria-label="Sequence-branch state"
+              className="relative h-6 w-48 text-gray-400 mx-auto justify-center action-state mb-[var(--vertical-offset)] flex items-center gap-3"
+            >
+              <span>
+                <MdNotInterested />
+              </span>
+              <span className="action-state__text">accepted</span>
+            </div>
+            {Object.keys(data.yes || {}).length ? (
+              <Nested data={data.yes} />
+            ) : (
+              <div>
+                <button>new sequence</button>
+              </div>
+            )}
           </div>
-        </TreeItemWrapper>
-        <ActionButton>
-          <span className="">Action</span>
-        </ActionButton>
-        <TreeItemWrapper>
-          <TimerButton label="1" />
-          <ActionState>
-            <span>
-              <MdNotInterested />
-            </span>
-            <span className="action-state__text">accepted</span>
-          </ActionState>
-          <div>
-            <Button>new sequence</Button>
+          {/* main brach */}
+          <div
+            role="tree-item"
+            className={cn(data.noDelay ? "action-face--fork" : "absolute left-[calc(50%-var(--block-w)/2)]")}
+          >
+            <div className="bg-purple-600 border border-purple-600 rounded-tr-md rounded-b-sm w-[var(--block-w)] h-10 text-center cursor-default relative z-20">
+              <button
+                data-test="msg-action-btn"
+                aria-label="Click to open sequence-action config popup"
+                aria-haspopup="true"
+                className="flex items-center h-full absolute left-0 top-0 w-[calc(100%-20px)] z-10 pl-5 text-white"
+              >
+                action
+              </button>
+            </div>
           </div>
-        </TreeItemWrapper>
-      </TwoBranchTree>
-    </Tree>
+          {/* main right */}
+          {data.noDelay && (
+            <div
+              role="tree-item"
+              className="relative flex flex-col items-center"
+            >
+              <div
+                aria-label="Sequence-branch state"
+                className="relative h-6 w-48 text-gray-400 mx-auto justify-center action-state mb-[var(--vertical-offset)] flex items-center gap-3"
+              >
+                <span>
+                  <MdNotInterested />
+                </span>
+                <span className="action-state__text">Not accepted</span>
+              </div>
+              <div className="relative">
+                <div className="w-40 action-delay relative mb-[var(--vertical-offset)] flex flex-col items-center">
+                  <button className="text-center px-5 flex items-center justify-center gap-2 bg-gray-300 p-3 rounded w-40 relative">
+                    <span className="absolute left-5">
+                      <RiTimerLine className="mr-5" />
+                    </span>
+                    <span>{data.noDelay?.value}</span>
+                    <span>{data.noDelay?.timeUnit}</span>
+                  </button>
+                </div>
+              </div>
+              {Object.keys(data.no || {}).length ? (
+                <Nested data={data.no} />
+              ) : (
+                <div>
+                  <button>new sequence</button>
+                </div>
+              )}
+            </div>
+          )}
+        </div>
+      </div>
+    </span>
   );
 }
 
--- app/page.tsx
+++ app/page.tsx
@@ -2,102 +2,254 @@
 import { Button } from "@/components/ui/button";
 import { useRef, useState } from "react";
 import { MdNotInterested } from "react-icons/md";
-import { RiTimerLine } from "react-icons/ri";
-import ActionState, {
+import {
   ActionButton,
   TimerButton,
   Tree,
   TreeItemWrapper,
   TwoBranchTree,
+  ActionState,
 } from "./tree";
 
+import React from "react"; // Assuming you're using react-icons for MdNotInterested
+import { RiTimerLine } from "react-icons/ri";
+import Nested from "./nested";
 
-import React from 'react';
-import { MdNotInterested } from 'react-icons/md'; // Assuming you're using react-icons for MdNotInterested
-
-// Define the structure of the tree and its branches
 const tree = {
-  type: "Tree",
-  branches: [
-    {
-      type: "TwoBranchTree",
-      items: [
-        {
-          type: "TreeItemWrapper",
-          actionState: {
-            icon: <MdNotInterested />, // Rendered icon
-            text: "accepted"
-          },
-          timerButton: {
-            label: "1"
-          },
-          tree:  {
-            type: "Tree",  // Nested Tree structure inside this item
-            branches: [
-              {
-                type: "TwoBranchTree",
-                items: [
-                  {
-                    type: "TreeItemWrapper",
-                    actionState: {
-                      icon: <MdNotInterested />,
-                      text: "accepted"
-                    },
-                    timerButton: {
-                      label: "1"
-                    }
-                  },
-                  {
-                    type: "ActionButton",
-                    label: "Action"
-                  },
-                  {
-                    type: "TreeItemWrapper",
-                    actionState: {
-                      icon: <MdNotInterested />,
-                      text: "accepted"
-                    },
-                    timerButton: {
-                      label: "1"
-                    },
-                    button: {
-                      label: "new sequence"
-                    }
-                  }
-                ]
-              }
-            ]
-          },
+  type: "CONNECT",
+  yesDelay: {
+    value: 1,
+    timeUnit: "DAYS",
+  },
+  noDelay: {
+    value: 1,
+    timeUnit: "DAYS",
+  },
+  message: "",
+  subject: null,
+  sequenceEndMode: null,
+  triggerType: "INVITE_ACCEPTED",
+  alternativeMessage: "",
+  alternativeSubject: null,
+  attachments: null,
+  yes: {
+    type: "MESSAGE",
+    yesDelay: {
+      value: 1,
+      timeUnit: "DAYS",
+    },
+    noDelay: null,
+    message: null,
+    subject: null,
+    sequenceEndMode: null,
+    triggerType: null,
+    alternativeMessage: "",
+    alternativeSubject: null,
+    attachments: {},
+    yes: {
+      type: "VISIT",
+      yesDelay: {
+        value: 1,
+        timeUnit: "DAYS",
+      },
+      noDelay: null,
+      message: null,
+      subject: null,
+      sequenceEndMode: null,
+      triggerType: null,
+      alternativeMessage: null,
+      alternativeSubject: null,
+      attachments: null,
+      yes: {
+        type: "ENDORSE",
+        yesDelay: {
+          value: 1,
+          timeUnit: "DAYS",
         },
-       
-        {
-          type: "ActionButton",
-          label: "Action"
+        noDelay: null,
+        message: null,
+        subject: null,
+        sequenceEndMode: null,
+        triggerType: null,
+        alternativeMessage: null,
+        alternativeSubject: null,
+        attachments: null,
+        yes: {
+          type: "FOLLOW",
+          yesDelay: {
+            value: 1,
+            timeUnit: "DAYS",
+          },
+          noDelay: null,
+          message: null,
+          subject: null,
+          sequenceEndMode: null,
+          triggerType: null,
+          alternativeMessage: null,
+          alternativeSubject: null,
+          attachments: null,
+          yes: {
+            type: "IF_EMAIL_AVAILABLE",
+            yesDelay: {
+              value: 0,
+              timeUnit: "DAYS",
+            },
+            noDelay: {
+              value: 0,
+              timeUnit: "DAYS",
+            },
+            message: null,
+            subject: null,
+            sequenceEndMode: null,
+            triggerType: null,
+            alternativeMessage: null,
+            alternativeSubject: null,
+            attachments: null,
+            yes: {
+              type: "EMAIL",
+              yesDelay: {
+                value: 1,
+                timeUnit: "DAYS",
+              },
+              noDelay: null,
+              message: "",
+              subject: "",
+              sequenceEndMode: null,
+              triggerType: null,
+              alternativeMessage: "",
+              alternativeSubject: "",
+              attachments: {},
+              yes: null,
+              no: null,
+            },
+            no: {
+              type: "FIND_EMAIL",
+              yesDelay: {
+                value: 0,
+                timeUnit: "DAYS",
+              },
+              noDelay: null,
+              message: null,
+              subject: null,
+              sequenceEndMode: null,
+              triggerType: null,
+              alternativeMessage: null,
+              alternativeSubject: null,
+              attachments: null,
+              yes: {
+                type: "IF_EMAIL_AVAILABLE",
+                yesDelay: {
+                  value: 0,
+                  timeUnit: "DAYS",
+                },
+                noDelay: {
+                  value: 0,
+                  timeUnit: "DAYS",
+                },
+                message: null,
+                subject: null,
+                sequenceEndMode: null,
+                triggerType: null,
+                alternativeMessage: null,
+                alternativeSubject: null,
+                attachments: null,
+                yes: null,
+                no: null,
+              },
+              no: null,
+            },
+          },
+          no: null,
         },
-        {
-          type: "TreeItemWrapper",
-          actionState: {
-            icon: <MdNotInterested />,
-            text: "accepted"
-          },
-          timerButton: {
-            label: "1"
-          },
-          button: {
-            label: "new sequence"
-          }
-        }
-      ]
-    }
-  ]
+        no: null,
+      },
+      no: null,
+    },
+    no: null,
+  },
+  no: {
+    type: "INMAIL",
+    yesDelay: {
+      value: 1,
+      timeUnit: "DAYS",
+    },
+    noDelay: {
+      value: 1,
+      timeUnit: "DAYS",
+    },
+    message: null,
+    subject: "",
+    sequenceEndMode: null,
+    triggerType: "MESSAGE_RESPONDED",
+    alternativeMessage: "",
+    alternativeSubject: "",
+    attachments: {},
+    yes: {
+      type: "WITHDRAW",
+      yesDelay: {
+        value: 21,
+        timeUnit: "DAYS",
+      },
+      noDelay: null,
+      message: null,
+      subject: null,
+      sequenceEndMode: null,
+      triggerType: null,
+      alternativeMessage: null,
+      alternativeSubject: null,
+      attachments: null,
+      yes: {
+        type: "CONNECT",
+        yesDelay: {
+          value: 1,
+          timeUnit: "DAYS",
+        },
+        noDelay: {
+          value: 1,
+          timeUnit: "DAYS",
+        },
+        message: "",
+        subject: null,
+        sequenceEndMode: null,
+        triggerType: "INVITE_ACCEPTED",
+        alternativeMessage: "",
+        alternativeSubject: null,
+        attachments: null,
+        yes: null,
+        no: null,
+      },
+      no: null,
+    },
+    no: {
+      type: "FOLLOW",
+      yesDelay: {
+        value: 1,
+        timeUnit: "DAYS",
+      },
+      noDelay: null,
+      message: null,
+      subject: null,
+      sequenceEndMode: null,
+      triggerType: null,
+      alternativeMessage: null,
+      alternativeSubject: null,
+      attachments: null,
+      yes: null,
+      no: null,
+    },
+  },
+  templateName: "news",
+  templateType: "INDIVIDUAL",
 };
-
-
-
 
 export default function Home() {
   // State to track if the user is currently dragging the element
   const [isDragging, setIsDragging] = useState(false);
+  const [sequence, setSequence] = useState([0]);
+
+  const handleAdd = () => {
+    setSequence((prevSequence) => [...prevSequence, 0]);
+  };
 
   // State to store the current position of the draggable element
   const [position, setPosition] = useState({ x: 0, y: 0 });
@@ -160,37 +312,7 @@ export default function Home() {
         }}
         onMouseDown={handleMouseDown} // Start dragging on mouse down
       >
-        <Tree>
-          <TwoBranchTree>
-            <TreeItemWrapper>
-              <ActionState>
-                <span>
-                  <MdNotInterested />
-                </span>
-                <span className="action-state__text">accepted</span>
-              </ActionState>
-              <TimerButton label="1" />
-              <div>
-                <Button>new sequence</Button>
-              </div>
-            </TreeItemWrapper>
-            <ActionButton>
-                <span className="">Action</span>
-              </ActionButton>
-            <TreeItemWrapper>
-              <TimerButton label="1" />
-              <ActionState>
-                <span>
-                  <MdNotInterested />
-                </span>
-                <span className="action-state__text">accepted</span>
-              </ActionState>
-              <div>
-                <Button>new sequence</Button>
-              </div>
-            </TreeItemWrapper>
-          </TwoBranchTree>
-        </Tree>
+        <Nested data={tree} />
       </div>
     </div>
   );
